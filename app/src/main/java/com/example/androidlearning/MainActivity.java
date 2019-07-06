3ccb32e873b183eee50bede775136044ab501a6d
aishwarya <aishwarya.a@techjini.com>
2019-07-06 13:20:04 +0530

    simple lambda

--- app/src/main/java/com/example/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/androidlearning/MainActivity.java
@@ -9,5 +9,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apicall(new Callback() {
+            @Override
+            public void result(int x) {
+                System.out.println(x);
+            }
+        });
+
+        apicall((x) -> System.out.println(x));
+
     }
+
+
+
+    public void apicall(Callback callback){
+        // do something (API call) long running
+        int x = 10;
+        callback.result(x);
+    }
+
+
+    interface Callback{
+        void result(int x);
+    }
+
+
+
 }

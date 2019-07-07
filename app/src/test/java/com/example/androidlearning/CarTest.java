1a18e23ae8fcba78cbc4a7bdc5f47c81ea17ea67
aishwarya <aishwarya.a@techjini.com>
2019-07-07 14:58:20 +0530

    Di basics

--- app/src/test/java/com/example/androidlearning/CarTest.java
+++ app/src/test/java/com/example/androidlearning/CarTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class CarTest {
 
 
-    
+
     Car car;
 
     @Test
@@ -22,27 +22,49 @@ public class CarTest {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         System.out.println(car);
 
+
+        Person person = new Person("Aish","10"); // eager init
+
+
+
+        Person p = new Person();
+
+        p.name = "Aish";
+
+
+        p.setName("Aish"); // lateinit
+
+
+
+
     }
+
+
+
+
+    class Person{
+        String name; // field
+        String id;
+
+        public Person() {
+        }
+
+        // constructor
+        public Person(String name, String id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        // method
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+
+
 
 }

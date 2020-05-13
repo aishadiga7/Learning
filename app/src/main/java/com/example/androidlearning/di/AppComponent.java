2089d4fbe2fef1a578b76969c6a94f4a6f17c62f
aishwarya <aishwaryadiga@gmail.com>
2020-05-13 20:01:39 +0530

    Add AppModule

--- app/src/main/java/com/example/androidlearning/di/AppComponent.java
+++ app/src/main/java/com/example/androidlearning/di/AppComponent.java
@@ -3,10 +3,12 @@ package com.example.androidlearning.di;
 import android.app.Application;
 import android.content.Context;
 
+import com.example.androidlearning.di.module.AppModule;
+
 import dagger.BindsInstance;
 import dagger.Component;
 
-@Component(modules = {})
+@Component(modules = {AppModule.class})
 public interface AppComponent {
 
     @Component.Builder
---
+++ app/src/main/java/com/example/androidlearning/di/module/AppModule.java
@@ -0,0 +1,9 @@
+package com.example.androidlearning.di.module;
+
+import dagger.Module;
+
+@Module
+public class AppModule {
+
+
+}

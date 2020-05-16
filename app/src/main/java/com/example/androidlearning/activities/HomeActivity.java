00b9fb8cfd854854c5f1b72ff6af7a5da91f65c5
aishwarya <aishwaryadiga@gmail.com>
2020-05-16 16:36:32 +0530

    Compoent builder

--- app/src/main/java/com/example/androidlearning/activities/HomeActivity.java
+++ app/src/main/java/com/example/androidlearning/activities/HomeActivity.java
@@ -1,26 +1,35 @@
 package com.example.androidlearning.activities;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.androidlearning.DaggerApplication;
 import com.example.androidlearning.R;
+import com.example.androidlearning.di.DaggerHomeActivityComponent;
 import com.example.androidlearning.di.HomeActivityComponent;
 import com.google.gson.Gson;
 
 import javax.inject.Inject;
 
+import dagger.android.DaggerActivity;
+
 public class HomeActivity extends AppCompatActivity {
     private static HomeActivityComponent activityComponent;
     private Gson gson;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-       // activityComponent = DaggerH
-        gson = activityComponent.provideGson();
+        activityComponent = DaggerHomeActivityComponent.builder()
+                .setFragmentManager(getSupportFragmentManager())
+                .setAppComponent(DaggerApplication.appComponent)
+                .build();
 
+        gson = activityComponent.provideGson();
+        fragmentManager = activityComponent.provideFragmentManager();
 
     }
 }
--- app/src/main/java/com/example/androidlearning/di/HomeActivityComponent.java
+++ app/src/main/java/com/example/androidlearning/di/HomeActivityComponent.java
@@ -1,6 +1,8 @@
 package com.example.androidlearning.di;
 
+import android.app.Application;
 import android.content.Context;
+import android.support.v4.app.FragmentManager;
 
 import com.example.androidlearning.di.scopes.ForActivityComponent;
 import com.google.gson.Gson;
@@ -15,9 +17,14 @@ import dagger.Component;
 public interface HomeActivityComponent {
     Gson provideGson();
 
+    FragmentManager provideFragmentManager();
+
     @Component.Builder
     interface Builder {
         HomeActivityComponent build();
+        @BindsInstance
+        HomeActivityComponent.Builder setFragmentManager(FragmentManager fragmentManager);
 
+        Builder setAppComponent(AppComponent appComponent);
     }
 }
